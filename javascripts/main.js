f10c5c8d4002e3453f9979b57e263c31d446073a
Anita Perala <anitaperala@gmail.com>
2015-09-13 14:05:24 -0400

    Add dropdown menu to navbar, add selected pill

--- javascripts/main.js
+++ javascripts/main.js
@@ -17,6 +17,28 @@ function ToggleElement(){
   });
 }
 
+var $select = $("<select></select>");
+$("nav").append($select);
+
+$("nav a").each(function(){
+  var $anchor = $(this);
+  var $option = $("<option></option>");
+
+  if($anchor.parent().hasClass("selected")) {
+    $option.prop("selected", true);
+  }
+
+  $option.val($anchor.attr("href"));
+  $option.text($anchor.text());
+  $select.append($option);
+});
+
+$select.change(function() {
+  window.location = $select.val();
+
+});
+
 $(document).ready(function() {
   ToggleElement();
+
 });
